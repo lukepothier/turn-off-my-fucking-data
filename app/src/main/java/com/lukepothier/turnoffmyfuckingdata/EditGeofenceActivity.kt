66c8198f64a6d17cbd3e2bc5cf8d54913da97733
Luke Pothier <lukepothier@gmail.com>
2018-02-12 20:07:51 +0200

    Add location edit screen

--- app/src/main/java/com/lukepothier/turnoffmyfuckingdata/EditGeofenceActivity.kt
+++ app/src/main/java/com/lukepothier/turnoffmyfuckingdata/EditGeofenceActivity.kt
@@ -3,8 +3,10 @@ package com.lukepothier.turnoffmyfuckingdata
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import com.google.gson.Gson
 
 import kotlinx.android.synthetic.main.activity_edit_geofence.*
+import kotlinx.android.synthetic.main.content_edit_geofence.*
 
 class EditGeofenceActivity : AppCompatActivity() {
 
@@ -13,10 +15,22 @@ class EditGeofenceActivity : AppCompatActivity() {
         setContentView(R.layout.activity_edit_geofence)
         setSupportActionBar(toolbar)
 
+        val extras = intent.extras
+        if (extras != null) {
+            val prefs = this.getSharedPreferences(resources.getString(R.string.preferences_filename), 0)
+            val geofence = prefs.getString(extras.getString("locationName"), "")
+            val gson = Gson()
+            val deserializedGeofence = gson.fromJson<Geofence>(geofence, Geofence::class.java)
+
+            editTextLocationName.setText(deserializedGeofence.name)
+            editTextLocationLatitude.setText(deserializedGeofence.location.latitude.toString())
+            editTextLocationLongitude.setText(deserializedGeofence.location.longitude.toString())
+            editTextLocationRadius.setText((deserializedGeofence.radiusMetres.toString()))
+        }
+
         fabSave.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
     }
-
 }
