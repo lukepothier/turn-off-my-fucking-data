4b9e4995f678b4061f2ddde657a33acd39bef3f9
Luke Pothier <lukepothier@gmail.com>
2018-02-12 18:47:22 +0200

    Add functional CancellableEditText

--- app/src/main/java/com/lukepothier/turnoffmyfuckingdata/CancellableEditText.kt
+++ app/src/main/java/com/lukepothier/turnoffmyfuckingdata/CancellableEditText.kt
@@ -5,14 +5,28 @@ import android.util.AttributeSet
 import android.view.KeyEvent
 import android.widget.EditText
 
+class CancellableEditText : EditText {
 
-class CancellableEditText (context: Context, attrs: AttributeSet)
-    : EditText(context, attrs) {
+    private var _listener: IOnBackButtonListener? = null
+
+    interface IOnBackButtonListener {
+        fun onEditTextBackButton(): Boolean
+    }
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    fun setOnBackButtonListener(l: IOnBackButtonListener) {
+        _listener = l
+    }
 
     override fun onKeyPreIme(keyCode: Int, event: KeyEvent): Boolean {
-        if (event.keyCode == KeyEvent.KEYCODE_BACK) {
-            return super.dispatchKeyEvent(event)
+        if (event.action == KeyEvent.ACTION_UP && keyCode == KeyEvent.KEYCODE_BACK) {
+            if (_listener != null && _listener!!.onEditTextBackButton())
+                return false
         }
-        return super.dispatchKeyEvent(event)
+
+        return super.onKeyPreIme(keyCode, event)
     }
 }
